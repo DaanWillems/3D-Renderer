8cae1fcd5b8b9fc1489157b2ff3415f5d2972d3f
Vincent Brouwers <vincent-9@live.nl>
2019-01-13 18:48:17 +0100

    Simple tests

--- test/math/vec4.cpp
+++ test/math/vec4.cpp
@@ -5,13 +5,54 @@
 #include <math/mat4.h>
 
 // Tests vec.
-TEST(vectorTest, NormalizeVector) {
+TEST(Vector4, NormalizeVector) {
   math::vec4 v{0, 0, 2};
   v = v.normalize();
-  EXPECT_EQ(v.length(), 1);
   EXPECT_EQ(v.x(), 0);
   EXPECT_EQ(v.y(), 0);
   EXPECT_EQ(v.z(), 1);
+}
+
+
+TEST(Vector4, CrossProduct) {
+  math::vec4 v1{0, 0, 2};
+  math::vec4 v2{0, 0, 2};
+
+  auto result {v1.cross(v2)};
+
+  EXPECT_EQ(result.x(), 0);
+  EXPECT_EQ(result.y(), 0);
+  EXPECT_EQ(result.z(), 0);
+
+  v1 = {1, 0, 2};
+  v2 = {0, 2, 0};
+
+  result = v1.cross(v2);
+
+  EXPECT_EQ(result.x(), -4);
+  EXPECT_EQ(result.y(), 0);
+  EXPECT_EQ(result.z(), 2);
+}
+
+
+TEST(Vector4, DotProduct2D) {
+  math::vec4 v1{1, 0};
+  math::vec4 v2{1, 0};
+  auto result{v1.dot(v2)};
+
+  EXPECT_EQ(result, 1);
+
+  v1 = {1, 0};
+  v2 = {0, 1};
+  result = v1.dot(v2);
+
+  EXPECT_EQ(result, 0);
+
+  v1 = {0, 1};
+  v2 = {0, 1};
+  result = v1.dot(v2);
+
+  EXPECT_EQ(result, 1);
 }
 
 
